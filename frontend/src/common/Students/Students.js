0bd00fa4b37eaae9e61d86a50b09dac966a9ca62
Ronak Shah <ronakshah2895@gmail.com>
2020-02-28 14:02:47 -0800

    Create students page prototype

--- frontend/src/common/Students/Students.js
+++ frontend/src/common/Students/Students.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import './Students.css';
 
 function Students() {
   return (
-    <h1>Students</h1>
+    <div className="STUDENT container">
+      <div className="row">
+        <div className="col-4">
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">Filters</h5>
+            </div>
+          </div>
+        </div>
+        <div className="col-8">
+          <div className="card">
+            <div className="card-body">
+              <img className="card-img" src="http://localhost:3001/images/default_profile_image.jpg" alt="" />
+              <h5 className="card-title">Ronak Shah</h5>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
-export default Students;
+export default connect()(Students);
