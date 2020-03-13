dc0c12aecfaa9310faaaa995363510eb89783abf
Ronak Shah <ronakshah2895@gmail.com>
2020-03-12 17:20:02 -0700

    Completed landing page ui

--- frontend/src/common/Landing/Landing.js
+++ frontend/src/common/Landing/Landing.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import './Landing.css';
 
 function Landing() {
   return (
-    <h1>Landing</h1>
+    <div className="LANDING container">
+      <div className="row image-row">
+        <div className="col image-col">
+          <img className="landing-img" src={`${process.env.REACT_APP_SERVER_ROOT}images/landing.jpg`} alt="" />
+        </div>
+        <div className="col">
+          <h1 className="landing-img-text">The #1 way college students find jobs</h1>
+        </div>
+      </div>
+    </div>
   );
 }
 
